71887ee95990f1461dd8d4da64fd9f928e6c2798
ZDubeau <zdubeau@dubeaul.com>
2021-09-20 21:29:09 +0200

    Clean code

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Home from "./components/Home";
-import BasicScreen from "./components/BasicScreen";
-import ImageScreen from "./components/ImageScreen";
-import Formiko from "./components/Formiko";
-import MapScreen from "./components/MapScreen";
-import MyMap from "./components/MyMap";
 
 export default function App() {
-  return <BasicScreen />;
+  return <Home />;
 }
